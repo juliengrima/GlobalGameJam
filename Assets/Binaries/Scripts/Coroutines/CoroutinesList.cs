57bfc6bb798a43bcbd8c9f14f9c91cb628a0fa8f
juliengrima <gogrinformatique@gmail.com>
2024-01-26 23:34:52 +0100

    add files

---
+++ Assets/Binaries/Scripts/Coroutines/CoroutinesList.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Manager
+{
+    public class CoroutinesList : MonoBehaviour
+    {
+        #region Coroutines
+        IEnumerator EndCoroutine()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
+
