13063e458305804152c9dfb18dfbb9d977623d94
Xwilarg <xwilarg@protonmail.com>
2024-01-27 13:15:44 +0100

    Generate point count in runtime

---
+++ Assets/Binaries/Prefabs/Player/Script/UserInterface/PlayerUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _pointContainer;
+
+    [SerializeField]
+    private GameObject _pointPrefab;
+
+    private readonly List<GameObject> _points = new();
+
+    private void Start()
+    {
+        for (int i = 0; i < GameManager.Instance.GameInfo.MaxItemCount; i++)
+        {
+            _points.Add(Instantiate(_pointPrefab, _pointContainer));
+        }
+    }
+
+    /// <returns>Is the player still alive</returns>
+    public bool TakeDamage()
+    {
+        if (!_points.Any()) return false;
+
+        _points.RemoveAt(0);
+        return _points.Any();
+    }
+}
--- Assets/Binaries/Scripts/Game/GameManager.cs
+++ Assets/Binaries/Scripts/Game/GameManager.cs
@@ -2,6 +2,11 @@
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameInfo _info;
+
+    public GameInfo GameInfo => _info;
+
     public static GameManager Instance { get; private set; }
 
     private void Awake()
