7397a94ec23a7f3b8aad622ab5d039b584970122
Xwilarg <xwilarg@protonmail.com>
2024-01-27 15:49:15 +0100

    Implement BGM

--- Assets/Binaries/Scripts/Evenements/EventManager.cs
+++ Assets/Binaries/Scripts/Evenements/EventManager.cs
@@ -1,8 +1,29 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
     public static EventManager Instance { private set; get; }
 
+    [SerializeField]
+    private GameObject _eventContainer;
+
+    [SerializeField]
+    private TMP_Text _title, _description;
+
     public bool AreKeysInverted;
+}
+
+public class EventInfo
+{
+    public EventInfo(string name, string description, System.Action onEnable, System.Action onDisable)
+    {
+        _name = name;
+        _description = description;
+        _onEnable = onEnable;
+        _onDisable = onDisable;
+    }
+
+    private string _name, _description;
+    private System.Action _onEnable, _onDisable;
 }
--- Assets/Binaries/Scripts/SO/GameInfo.cs
+++ Assets/Binaries/Scripts/SO/GameInfo.cs
@@ -7,4 +7,12 @@ public class GameInfo : ScriptableObject
     [Tooltip("Duration of the countdown until the game start")] public int CountdownDuration;
     [Tooltip("Max duration of the game in seconds")] public float GameDuration;
     [Tooltip("Minimum amount of players to start the game")] public int MinPlayerRequirement;
+    [Tooltip("Min/Max duration between events")] public MinMax<float> EventInterval;
+    [Tooltip("Min/Max duration of an event")] public MinMax<float> EventDuration;
+}
+
+[System.Serializable]
+public class MinMax<T>
+{
+    public T Min, Max;
 }
