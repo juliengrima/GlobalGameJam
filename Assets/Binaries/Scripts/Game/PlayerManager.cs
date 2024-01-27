c70ea2ba6f0a8a75e858b2fc1d5e400e3b7bf9d7
Xwilarg <xwilarg@protonmail.com>
2024-01-27 11:20:04 +0100

    Add player on join

--- Assets/Binaries/Scripts/Game/PlayerManager.cs
+++ Assets/Binaries/Scripts/Game/PlayerManager.cs
@@ -1,17 +1,22 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
     private Transform[] _spawnPoints;
+
+    private readonly List<GameObject> _players = new();
 
     private void Awake()
     {
         _spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint").Select(x => x.transform).ToArray();
     }
 
-    public void OnPlayJoin()
+    public void OnPlayJoin(PlayerInput pi)
     {
-
+        pi.transform.position = _spawnPoints[_players.Count].position;
+        _players.Add(pi.gameObject);
     }
 }
