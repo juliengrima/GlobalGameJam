e73feb13602db477c9debe1e32a10f39289ec187
juliengrima <gogrinformatique@gmail.com>
2024-01-27 10:41:36 +0100

    Last controls player

--- Assets/Binaries/Prefabs/Player/Script/Entities/EntityMove.cs
+++ Assets/Binaries/Prefabs/Player/Script/Entities/EntityMove.cs
@@ -26,21 +26,18 @@ public class EntityMove : MonoBehaviour
     [SerializeField] float _speed;
     [SerializeField] float _jumpHeight;
     [SerializeField] float _rotation;
-    [SerializeField] float _rotationSpeed;
-    [SerializeField] float _rotationThreshold;
+    [SerializeField, Range(1, 5)] float _rotationSpeed;
     [SerializeField] float _smoothTime;
     //[SerializeField] int _jumpDamage;
     [SerializeField, Range(0, -11)] float _gravity;
     //Privates Components
     Vector3 playerVelocity;
     Vector3 _direction;
-    Coroutine _rotationCoroutine;
     //Privates Fields
     bool _IsJumping;
     bool groundedPlayer;
     float _currentVelocity;
-    float _angle;
-    float _targetAngle;
+
    
 
     public AudioSource Source { get => _source; set => _source = value; }
@@ -51,7 +48,6 @@ public class EntityMove : MonoBehaviour
     public float SmoothTime { get => _smoothTime; set => _smoothTime = value; }
     public float Gravity { get => _gravity; set => _gravity = value; }
     public float CurrentVelocity { get => _currentVelocity; set => _currentVelocity = value; }
-    public float Angle { get => _angle; set => _angle = value; }
     #endregion
     #region Unity LifeCycle
     // Start is called before the first frame update
@@ -85,25 +81,20 @@ public class EntityMove : MonoBehaviour
 
         _direction = moving;
 
-        if (_direction.sqrMagnitude == 0) return; 
-        _targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
-        _angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetAngle, ref _currentVelocity, _smoothTime);
-        Quaternion rotateController = _controller.transform.rotation = Quaternion.Euler(0, _angle, 0);
-        //_rotationCoroutine = StartCoroutine(_coroutines.MoveCoroutine(this));
+        if (_direction.sqrMagnitude == 0) return;
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(_controller.transform.eulerAngles.y, targetAngle, ref _currentVelocity, _smoothTime);
 
-        if (Quaternion.Angle(transform.rotation, rotateController) < _rotationThreshold)
+        //_playerStateMachine.Move
+
+        _controller.transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        Vector3 move = new Vector3(_direction.x, 0, _direction.y);
+        _controller.Move(move * Time.deltaTime * _speed);
+
+        if (move != Vector3.zero)
         {
-            // Si la rotation est terminée, permettre au personnage d'avancer
-            //_controller.Move(transform.forward * _speed * Time.deltaTime);
-
-            //CharacterController Movements
-            Vector3 move = new Vector3(_direction.x, 0, _direction.y);
-            _controller.Move(move * Time.deltaTime * _speed);
-
-            if (move != Vector3.zero)
-            {
-                gameObject.transform.forward = move;
-            }
+            gameObject.transform.forward = move;
         }
 
         // Changes the height position of the player..
