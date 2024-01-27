3f49050ffc50e38400d6ba597b6833e3078d44b9
Xwilarg <xwilarg@protonmail.com>
2024-01-27 15:56:43 +0100

    Start event management

--- Assets/Binaries/Scripts/Evenements/EventManager.cs
+++ Assets/Binaries/Scripts/Evenements/EventManager.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
@@ -12,6 +13,27 @@ public class EventManager : MonoBehaviour
     private TMP_Text _title, _description;
 
     public bool AreKeysInverted;
+
+    private void Awake()
+    {
+        StartCoroutine(RunEvents());
+    }
+
+    private IEnumerator RunEvents()
+    {
+        while (true)
+        {
+            if (GameManager.Instance.CanPlay)
+            {
+                yield return new WaitForSeconds(Random.Range(GameManager.Instance.GameInfo.EventInterval.Min, GameManager.Instance.GameInfo.EventInterval.Max));
+                yield return new WaitForSeconds(Random.Range(GameManager.Instance.GameInfo.EventDuration.Min, GameManager.Instance.GameInfo.EventDuration.Max));
+            }
+            else
+            {
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
 }
 
 public class EventInfo
